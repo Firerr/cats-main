62e6c7c117a9bd632e5087a9d97d8b32de440cf9
Firerr <steelejames022@gmail.com>
2024-12-18 19:53:37 +0000

    fixed db url

--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,18 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const { MONGODB_URI = "mongodb://127.0.0.1/cats" } = process.env;
-console.log("connecting db", MONGODB_URI);
+const {
+  MONGODB_URI = 'mongodb+srv://steelejames022:ugsyLn4rGnDhpQkF@cluster0.kdb0c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/cats',
+} = process.env;
+console.log('connecting db', MONGODB_URI);
 
 const conn = mongoose
   .connect(MONGODB_URI)
   .then(function () {
-    console.log("DB Connected");
+    console.log('DB Connected');
   })
   .catch(function (error) {
-    console.log('line check')
-    console.log("Error connecting to DB", error);
+    console.log('line check');
+    console.log('Error connecting to DB', error);
   });
 
 export default conn;
-
